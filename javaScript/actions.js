3acb85c2d554a06263c51689be6f23c94a3216da
Guillermo2830 <ingguillermorojas30@gmail.com>
2024-10-21 16:41:07 -0500

    Creation of the "Header"

---
+++ javaScript/actions.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    var menuContactContainer = document.querySelector('.menu-contact-container');
+
+    menuContactContainer.classList.toggle('active');
+});
