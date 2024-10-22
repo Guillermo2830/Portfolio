f1bc3b8c8cbd39fcc4eefb604e166ccc6f6ee02d
Guillermo2830 <ingguillermorojas30@gmail.com>
2024-10-22 11:28:58 -0500

    Actions to the contact bottom

--- javaScript/actions.js
+++ javaScript/actions.js
@@ -3,3 +3,23 @@ document.getElementById('hamburger').addEventListener('click', function () {
 
     menuContactContainer.classList.toggle('active');
 });
+
+//Control the color of the class contact
+
+document.getElementById('contact').addEventListener('click', function () {
+    this.classList.add('active')
+
+    localStorage.setItem('contactClicked', 'true')
+})
+
+window.addEventListener('load', function() {
+    if (localStorage.getItem('contactClicked') === 'true') {
+        document.getElementById('contact').classList.add('active');
+    }
+});
+
+window.addEventListener('beforeunload', function() {
+    if (window.location.pathname === '/pages/contact.html') {
+        localStorage.removeItem('contactClicked');
+    }
+});
